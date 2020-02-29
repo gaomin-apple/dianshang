3d0cf9df1b0bbcbe5644078ba501e7332ac74f01
gaomin <2226959102@qq.com>
2020-02-29 20:25:06 +0800

    add store page and js

---
+++ sxy/jcart-store-front/viewmodels/product-search.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        ProductList: {},
+        pageNum: 1
+    },
+    mounted(){
+        console.log('view mounted');
+        this.searchProduct();
+    },
+    methods: {
+        searchProduct(){
+            axios.get('/product/search',{
+                params: {
+                    pageNum:this.pageNum
+                }
+            })
+            .then(function (response){
+                console.log(response);
+                app.ProductList = response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        },
+        handlePageChange(val){
+            console.log('page change',val);
+            this.pageNum = val;
+            this.searchProduct();
+        }
+    }
+})

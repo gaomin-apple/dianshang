0ef67ebdc355072870fb4a858f19fdeadaf9be0e
gaomin <2226959102@qq.com>
2020-03-02 15:36:23 +0800

    add admin delete page

--- sxy/jcart-administration-front/viewmodels/administrator-index.js
+++ sxy/jcart-administration-front/viewmodels/administrator-index.js
@@ -2,19 +2,68 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: {},
-        pageNum: 1
+        pageNum: 1,
+        selectedAdministrators: []
     },
-    mounted(){
+    computed: {
+        selectedAdministrators() {
+            return this.selectedAdministrators.map(a => a.administratorId);
+        }
+    },
+    mounted() {
         console.log('view mounted');
         this.getAdministrators();
     },
     methods: {
-        handlePageChange(val){
+        handlePageChange(val) {
             console.log('page change',val);
             this.pageNum = val;
             this.getAdministrators();
         },
-        getAdministrators(){
+        handleDelete(index, row) {
+            console.log('delete click');
+
+            if (confirm("确认删除？")) {
+                this.deleteAdministrator(row.administratorId);
+            }
+        },
+        deleteAdministrator(administratorId) {
+            axios.post('/administrator/delete', administratorId, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    alert('删除成功');
+                    location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        handleBatchDeleteClick() {
+            console.log('batch delete click');
+            if (confirm("确认删除？")) {
+                this.batchDeleteAdministrators();
+            }
+        },
+        handleSelectionChange(val) {
+            console.log('selection change', val);
+            this.selectedAdministrators = val;
+        },
+        batchDeleteAdministrators() {
+            axios.post('/administrator/batchDelete', this.selectedAdministratorIds)
+                .then(function (response) {
+                    console.log(response);
+                    alert('批删成功');
+                    location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        getAdministrators() {
             axios.get('/administrator/getList', {
                 params: {
                     pageNum: this.pageNum

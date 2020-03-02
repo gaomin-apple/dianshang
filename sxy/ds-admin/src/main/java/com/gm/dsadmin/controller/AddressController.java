ca54ba8910b97f87fb7450dfc6c8254f95fc9205
gaomin <2226959102@qq.com>
2020-03-02 15:45:45 +0800

    add address pages

--- sxy/ds-admin/src/main/java/com/gm/dsadmin/controller/AddressController.java
+++ sxy/ds-admin/src/main/java/com/gm/dsadmin/controller/AddressController.java
@@ -1,15 +1,13 @@
 package com.gm.dsadmin.controller;
 
 import com.gm.dsadmin.dto.out.AddressListOutDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/address")
+@CrossOrigin
 public class AddressController {
 
     @GetMapping("/getListByCustomerId")

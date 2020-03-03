c458cf8cf2810db77c6671192ad7e407c3b1f8fb
gaomin <2226959102@qq.com>
2020-03-03 14:41:58 +0800

    add shopping delete page

--- sxy/jcart-store-front/viewmodels/shopping-cart.js
+++ sxy/jcart-store-front/viewmodels/shopping-cart.js
@@ -7,5 +7,12 @@ var app = new Vue({
         console.log('view mounted');
         var myshoppingCartJson = localStorage['myShoppingCartJson'];
         this.myShoppingCart = JSON.parse(myshoppingCartJson);
+    },
+    methods: {
+        handleDelete(index, row) {
+            console.log('delete click');
+            this.myShoppingCart.splice(index,1);
+            localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+        }
     }
 })

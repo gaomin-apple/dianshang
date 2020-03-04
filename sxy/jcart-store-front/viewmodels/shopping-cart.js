ed30626cc821a4e6accd2c17088fed3f6f654aaa
gaomin <2226959102@qq.com>
2020-03-04 15:58:36 +0800

    add shopping-cart page

--- sxy/jcart-store-front/viewmodels/shopping-cart.js
+++ sxy/jcart-store-front/viewmodels/shopping-cart.js
@@ -31,6 +31,11 @@ var app = new Vue({
             console.log('update click');
             localStorage['myShopping'] = JSON.stringify(this.myShoppingCart);
             this.$message.success('修改购物车成功');
+        },
+        handleClearCart() {
+            console.log('clear cart click');
+            this.myShoppingCart = [];
+            localStorage.removeItem('myShoppingCartJson');
         }
     }
 })

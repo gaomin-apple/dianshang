0d6c491c4db7e60c96a8c3806ffde52fd6fab0fd
gaomin <2226959102@qq.com>
2020-03-05 20:32:54 +0800

    add order show api

--- sxy/ds-admin/src/main/java/com/gm/dsadmin/service/impl/OrderServiceImpl.java
+++ sxy/ds-admin/src/main/java/com/gm/dsadmin/service/impl/OrderServiceImpl.java
@@ -1,21 +1,30 @@
 package com.gm.dsadmin.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.gm.dsadmin.dao.OrderDetailMapper;
 import com.gm.dsadmin.dao.OrderMapper;
 import com.gm.dsadmin.dto.out.OrderListOutDTO;
 import com.gm.dsadmin.dto.out.OrderShowOutDTO;
+import com.gm.dsadmin.po.Customer;
 import com.gm.dsadmin.po.Order;
 import com.gm.dsadmin.po.OrderDetail;
+import com.gm.dsadmin.service.CustomerService;
 import com.gm.dsadmin.service.OrderService;
+import com.gm.dsadmin.vo.OrderProductVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderMapper orderMapper;
+
+    @Autowired
+    private CustomerService customerService;
 
     @Autowired
     private OrderDetailMapper orderDetailMapper;
@@ -35,6 +44,25 @@ public class OrderServiceImpl implements OrderService {
         OrderShowOutDTO orderShowOutDTO = new OrderShowOutDTO();
         orderShowOutDTO.setOrderId(orderId);
         orderShowOutDTO.setCustomerId(order.getCustomerId());
-        return null;
+
+        Customer customer = customerService.getById(order.getCustomerId());
+        orderShowOutDTO.setCustomerName(customer.getRealName());
+        orderShowOutDTO.setStatus(customer.getStatus());
+        orderShowOutDTO.setTotalPrice(order.getTotalPrice());
+        orderShowOutDTO.setRewordPoints(order.getRewordPoints());
+        orderShowOutDTO.setCreateTimestamp(order.getCreateTime().getTime());
+        orderShowOutDTO.setUpdateTimestamp(order.getUpdateTime().getTime());
+
+        orderShowOutDTO.setShipMethod(orderDetail.getShipMethod());
+        orderShowOutDTO.setShipAddress(orderDetail.getShipAddress());
+        orderShowOutDTO.setShipPrice(orderDetail.getShipPrice());
+        orderShowOutDTO.setPayMethod(orderDetail.getPayMethod());
+        orderShowOutDTO.setInvoiceAddress(orderDetail.getInvoiceAddress());
+        orderShowOutDTO.setInvoicePrice(orderDetail.getInvoicePrice());
+        orderShowOutDTO.setComment(orderDetail.getComment());
+
+        List<OrderProductVO> orderProductVOS = JSON.parseArray(orderDetail.getOrderProducts(), OrderProductVO.class);
+        orderShowOutDTO.setOrderProducts(orderProductVOS);
+        return orderShowOutDTO;
     }
 }

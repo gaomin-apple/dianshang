2d404186507b2a0c33615b45e83c0a7a40f385e7
gaomin <2226959102@qq.com>
2020-03-08 17:27:29 +0800

    add admin return history create api

--- sxy/ds-admin/src/main/java/com/gm/dsadmin/service/impl/ReturnHistoryServiceImpl.java
+++ sxy/ds-admin/src/main/java/com/gm/dsadmin/service/impl/ReturnHistoryServiceImpl.java
@@ -1,18 +1,23 @@
 package com.gm.dsadmin.service.impl;
 
 import com.gm.dsadmin.dao.ReturnHistoryMapper;
+import com.gm.dsadmin.po.Return;
 import com.gm.dsadmin.po.ReturnHistory;
 import com.gm.dsadmin.service.ReturnHistoryService;
+import com.gm.dsadmin.service.ReturnService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class ReturnHistoryServiceImpl implements ReturnHistoryService {
     @Autowired
     private ReturnHistoryMapper returnHistoryMapper;
-
+    @Autowired
+    private ReturnService returnService;
     @Override
     public List<ReturnHistory> getListByReturnId(Integer returnId) {
         List<ReturnHistory> returnHistories = returnHistoryMapper.selectListByReturnId(returnId);
@@ -20,9 +25,15 @@ public class ReturnHistoryServiceImpl implements ReturnHistoryService {
     }
 
     @Override
+    @Transactional
     public Long create(ReturnHistory returnHistory) {
         returnHistoryMapper.insertSelective(returnHistory);
         Long returnHistoryId = returnHistory.getReturnHistoryId();
+
+        Return aReturn = new Return();
+        aReturn.setReturnId(returnHistory.getReturnId());
+        aReturn.setUpdateTime(new Date());
+        returnService.update(aReturn);
         return returnHistoryId;
     }
 }

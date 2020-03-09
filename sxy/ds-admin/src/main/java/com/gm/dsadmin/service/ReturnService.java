8986e8c2bcd31141c23555bfcec5c82a782954f9
gaomin <2226959102@qq.com>
2020-03-09 20:33:38 +0800

    add admin return search api

--- sxy/ds-admin/src/main/java/com/gm/dsadmin/service/ReturnService.java
+++ sxy/ds-admin/src/main/java/com/gm/dsadmin/service/ReturnService.java
@@ -1,10 +1,11 @@
 package com.gm.dsadmin.service;
 
 import com.github.pagehelper.Page;
+import com.gm.dsadmin.dto.in.ReturnSearchInDTO;
 import com.gm.dsadmin.po.Return;
 
 public interface ReturnService {
-    Page<Return> search(Integer pageNum);
+    Page<Return> search(ReturnSearchInDTO returnSearchInDTO,Integer pageNum);
 
     Return getById(Integer returnId);
 

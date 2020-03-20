dfeb3d2202463832be24a18c5790b09598798a4b
gaomin <2226959102@qq.com>
2020-03-20 17:28:11 +0800

    add 计时器

--- sxy/jcart-administration-front/viewmodels/administrator-forget-pwd.js
+++ sxy/jcart-administration-front/viewmodels/administrator-forget-pwd.js
@@ -2,12 +2,24 @@ var app = new Vue({
     el: '#app',
     data: {
         email: '',
-        loading:false
+        loading: false,
+        buttonEnabled: true,
+        counter:120
     },
     methods: {
         handleFindBackPwdClick() {
             console.log('find back pwd click');
             this.loading = true;
+            this.buttonEnabled = false;
+            this.counter = 120;
+
+            setInterval(function () {
+                console.log('count down');
+                app.counter--;
+                if(counter <0) {
+                    app.buttonEnabled = true;
+                }
+            },1000);
             this.getPwdResetCode();
         },
         getPwdResetCode() {
@@ -19,7 +31,7 @@ var app = new Vue({
                 .then(function (response) {
                     console.log(response);
                     app.loading = false;
-                    alert('重置码已发送到邮箱');
+                    //alert('重置码已发送到邮箱');
                 })
                 .catch(function (error) {
                     console.log(error);
